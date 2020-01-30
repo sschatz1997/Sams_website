591e132db931b0d45288a0880a5ccb4c31447d42
sschatz1997 <s.schatz1997@gmail.com>
2020-01-30 13:35:23 -0500

    decode

--- Website Entry/file-IO-Test.php
+++ Website Entry/file-IO-Test.php
@@ -1,10 +1,10 @@
 <?php
 $file = fopen("test.txt","r") or die("Unable to open file!");
-$pwd = fread($file, filesize("/home/sam/pull/test.txt"));
+$pwd = fread($file, filesize("test.txt"));
 echo "Non-base64\n";
 echo $pwd . "\n";
 echo "Base64\n";
-$b64 = base64_encode($pwd);
+$b64 = base64_decode($pwd);
 echo $b64 . "\n";
 
 fclose($file);
