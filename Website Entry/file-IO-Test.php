2005e18b26a3997b85fb99d80cd6c3bdb6f53dd6
sschatz1997 <s.schatz1997@gmail.com>
2020-01-30 13:33:43 -0500

    base 64

--- Website Entry/file-IO-Test.php
+++ Website Entry/file-IO-Test.php
@@ -1,6 +1,12 @@
 <?php
 $file = fopen("test.txt","r") or die("Unable to open file!");
-echo fread($file, filesize("/home/sam/pull/test.txt"));
+$pwd = fread($file, filesize("/home/sam/pull/test.txt"));
+echo "Non-base64\n";
+echo $pwd . "\n";
+echo "Base64\n";
+$b64 = base64_encode($pwd);
+echo $b64 . "\n";
+
 fclose($file);
 
 ?>
