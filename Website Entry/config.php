19785cd96e8e5c379a63de59c4fb992ba2734535
sschatz1997 <s.schatz1997@gmail.com>
2020-01-30 14:01:20 -0500

    base64

--- Website Entry/config.php
+++ Website Entry/config.php
@@ -1,7 +1,8 @@
 <?php
 
-$file = fopen("test.txt","r") or die("Unable to open file!");
-$pwd = fread($file, filesize("test.txt"));
+$file = fopen("/home/sam/pull/test.txt","r") or die("Unable to open file!");
+$pwd = fread($file, filesize("/home/sam/pull/test.txt"));
+$PASSWORD = base64_decode($pwd);
 
 $HOSTNAME = "localhost";
 //$PORT = 3306;
