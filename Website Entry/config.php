cc1ad3f2e85b85abc1e459ff0f89687c49b2f692
sschatz1997 <s.schatz1997@gmail.com>
2020-01-30 13:45:54 -0500

    d

--- Website Entry/config.php
+++ Website Entry/config.php
@@ -1,9 +1,11 @@
 <?php
 
+$file = fopen("test.txt","r") or die("Unable to open file!");
+$pwd = fread($file, filesize("test.txt"));
+
 $HOSTNAME = "localhost";
 //$PORT = 3306;
-$USERNAME = "remoteU";
-//$PASSWORD = "mmq78NYj8GtRVnMAF4ZE";
+$USERNAME = "localUser1";
 $DATABASE = "main";
 
 ?>
--- Website Entry/file-IO-Test.php
+++ Website Entry/file-IO-Test.php
@@ -1,10 +1,10 @@
 <?php
-$file = fopen("test.txt","r") or die("Unable to open file!");
+$file = fopen("/home/sam/pull/test.txt","r") or die("Unable to open file!");
 $pwd = fread($file, filesize("test.txt"));
 echo "Non-base64\n";
 echo $pwd . "\n";
 echo "Base64\n";
-$b64 = base64_decode($pwd);
+$b64 = base64_encode($pwd);
 echo $b64 . "\n";
 
 fclose($file);
