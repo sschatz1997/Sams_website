a51a0e6f6c2bb2a84626d8fc7cf53a18442b60aa
sschatz1997 <s.schatz1997@gmail.com>
2020-02-04 01:39:29 -0500

    Update login.php

--- Website Entry/login.php
+++ Website Entry/login.php
@@ -192,7 +192,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                     // Display an error message if username doesn't exist
                     $username_err = "Login Failed";
 		    $time = time();
-		    failed($time, $username, $password);
+            failed($time, $username, $password);
+            failed2($username);
                 }
             } else{
                 echo "Oops! Something went wrong. Please try again later.";
