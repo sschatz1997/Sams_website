a3612fd33582354ef15df4dcaf1a0bb67c6996d5
sschatz1997 <s.schatz1997@gmail.com>
2020-02-03 23:05:26 -0500

    edit config

--- Website Entry/config.php
+++ Website Entry/config.php
@@ -1,8 +1,8 @@
 <?php
 ini_set('display_errors', 'On');
 error_reporting(E_ALL);
-$file = fopen("/home/sam/pull/test.txt","r") or die("Unable to open file!");
-$pwd = fread($file, filesize("/home/sam/pull/test.txt"));
+$file = fopen("test.txt","r") or die("Unable to open file!");
+$pwd = fread($file, filesize("test.txt"));
 $PASSWORD = base64_decode($pwd);
 
 $HOSTNAME = "localhost";
