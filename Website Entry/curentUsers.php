3f37449f05ecbdfcfcb902dd559623bb84b7d8dd
sschatz1997 <s.schatz1997@gmail.com>
2020-02-04 01:00:53 -0500

    added table

---
+++ Website Entry/curentUsers.php
@@ -0,0 +1,65 @@
+<?php
+require_once "config.php";
+
+$loop = $con->query('SELECT id from users order by id desc limit 1;');
+$loop->execute();
+$entries = $loop->fetch(PDO::FETCH_BOTH);
+$entries = intval(array_pop($entries));
+
+$loop1 = $con->query('SELECT id from logins1 order by id desc limit 1;');
+$loop1->execute();
+$entries2 = $loop->fetch(PDO::FETCH_BOTH);
+$entries2 = intval($entries);
+
+$prep = $con->query('SELECT username FROM users;');
+$prep->execute();
+//$name = $prep->fetch_array(PDO::FETCH_BOTH);
+
+$prep2 = $con->prepare('SELECT created_at FROM users WHERE username = ?;');
+//$prep3 = $con->prepare('SELECT time1 FROM logins1 WHERE username = ?;');
+$prep3 = $con->prepare('SELECT id FROM logins1 WHERE username = ? ORDER BY id DESC LIMIT 1;');
+$prep4 = $con->prepare('SELECT time1 FROM logins1 WHERE id = ?;');
+$prep5 = $con->prepare('SELECT ip FROM logins1 WHERE id = ?;');
+
+//echo "entries: " . $entries;
+
+$x = 0;
+$masterTime = [];
+//echo "<th>".$tt."<th>";
+while($x < $entries){
+	
+	$name = $prep->fetch(PDO::FETCH_BOTH);
+	$un = array_pop($name);
+	$prep2->bindParam(1,$un,PDO::PARAM_STR, 50);
+	$prep2->execute();
+	$date1 = $prep2->fetch(PDO::FETCH_BOTH);
+	$date = array_pop($date1);
+	
+	$prep3->bindParam(1,$un,PDO::PARAM_STR, 50);
+	$prep3->execute();
+	$id = $prep3->fetch(PDO::FETCH_BOTH);
+	$id = intval(array_pop($id));
+
+	$prep4->bindParam(1,$id,PDO::PARAM_INT);
+	$prep4->execute();
+	$time1 = $prep4->fetch(PDO::FETCH_BOTH);
+    $time1 = array_pop($time1);
+    
+    $prep5->bindParam(1,$id,PDO::PARAM_INT);
+    $prep5->execute();
+    $ip = $prep5>fetch(PDO::FETCH_BOTH);
+    $ip = array_pop($ip);
+	
+	echo"<tr>";
+	echo "<th>".$un."</th>";
+	echo "<th>".$date."</th>";
+    echo "<th>".$time1."</th>";
+    echo "<th>".$ip."</th>";
+	echo"</tr>";
+	$x++;
+} 
+
+$test1 = "test1";
+
+
+?>
---
+++ Website Entry/dashboard.php
@@ -0,0 +1,80 @@
+<?php
+session_start();
+
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+	header("location: login.php");
+	exit;
+}
+
+require_once "config.php";
+//require_once "setIntialStatus.php";
+
+function getIP()
+{
+	if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+	    $ip = $_SERVER['HTTP_CLIENT_IP'];
+	} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+	    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+	} else {
+	    $ip = $_SERVER['REMOTE_ADDR'];
+	}
+	return $ip;
+}
+
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>sams page</title>
+	<hr>
+	<center><h1><strong>sams site</center></h1></center>
+	<link rel="stylesheet" href="landingPage1.css">
+	<link rel="stylesheet" href="dropdown.css">
+	<link rel="stylesheet" href="main.css">
+	<hr>
+</head>
+
+<body>
+	<?php setcookie("gotyounow",$_SESSION["username"]);?>
+	<hr>
+	<div class="navbar">
+		<a href="home.php">Home</a>
+		<a href="home.php">Profile</a>
+		<div class="dropdown">
+			<button class="dropbtn">Study forms
+				<i class="fa fa-caret-down"></i>
+			</button>
+			<div class="dropdown-content">
+				<a href="bb.php">Bug Bountiy Entry</a>
+			</div>
+		</div>
+		<?php if(htmlspecialchars($_SESSION["username"]) == "admin"){echo"<a href='home.php'>Dashboard</a>";}?>
+		<div class="1">
+			<form action="logout.php" method="POST">
+				<center><input type="submit" value="logout" name="logout"></span></center>
+			</form>
+		</div>
+    </div>
+    <div class="wrapper">
+		<div id="center"><!--change to left later-->
+			<table>
+                <tr>
+					<th>Users</th>
+					<th>Date Created</th>
+                    <th>Last Log-in</th>
+                    <th>IP address</th>
+                </tr>
+                <?php require_once "curentUsers.php";?>
+            </table>
+        </div>
+
+
+
+    </div>
+    
+</body>
+</html>
