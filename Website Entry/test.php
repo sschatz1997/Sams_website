8a0b7f64ba50700bc3bd8a726ad845f1c3311f53
sschatz1997 <s.schatz1997@gmail.com>
2020-02-04 13:43:22 -0500

    Update test.php

--- Website Entry/test.php
+++ Website Entry/test.php
@@ -3,12 +3,28 @@ ini_set('display_errors', 'On');
 error_reporting(E_ALL);
 require_once "config.php";
 
+function getIP()
+{
+	if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+	    $ip = $_SERVER['HTTP_CLIENT_IP'];
+	} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+	    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+	} else {
+	    $ip = $_SERVER['REMOTE_ADDR'];
+	}
+	return $ip;
+} 
+
+$ip = getIP();
+
 //function failed3(usrname)
 $usr = "sam";
 $prep1 = $con->prepare("SELECT atempts FROM logginAttempts WHERE username = ?");
 $prep1 -> bindParam(1,$usr,PDO::PARAM_STR,50);
 $prep1 -> execute();
 $attempts = $prep1->fetch(PDO::FETCH_BOTH);
+
+echo "The type for ip address is " . gettype($ip) . "\n";
 
 if(is_bool($attempts)){
 	echo "this works<br>";
