d35da731e88f941ea5897cb4bdc93376000cd37b
sschatz1997 <s.schatz1997@gmail.com>
2020-02-04 13:34:23 -0500

    test

--- Website Entry/test.php
+++ Website Entry/test.php
@@ -4,15 +4,20 @@ error_reporting(E_ALL);
 require_once "config.php";
 
 //function failed3(usrname)
-$usr = "sam"
-$ip = getIP();
+$usr = "sam";
 $prep1 = $con->prepare("SELECT atempts FROM logginAttempts WHERE username = ?");
 $prep1 -> bindParam(1,$usr,PDO::PARAM_STR,50);
 $prep1 -> execute();
 $attempts = $prep1->fetch(PDO::FETCH_BOTH);
 
-echo "The type is: " . gettype($attempts) . "\n";
-echo "attempts " . $attempts;    
+if(is_bool($attempts)){
+	echo "this works<br>";
+} else {
+	echo "it doesnt<br>";
+}
+
+/*echo "The type is: " . gettype($attempts) . "\n";
+echo "attempts " . $attempts;    */
 
 
 ?>
