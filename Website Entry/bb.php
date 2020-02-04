27acadd24448363194b0372f5d22bb3d4b0c998f
sschatz1997 <s.schatz1997@gmail.com>
2020-02-04 00:24:39 -0500

    bb.php is done

--- Website Entry/bb.php
+++ Website Entry/bb.php
@@ -8,4 +8,90 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 
 require_once "config.php";
 
+function getID()
+{
+	$db1 = new myDB();
+	$result = $db1->query('SELECT id FROM companiesBasic order by id desc limit 1;');
+	//$result->execute();
+	$results = $result->fetchArray();
+	$id = intval(array_pop($results));
+	return $id+1;
+}
+
+$id = getID();
+$name = $_POST['name'];
+$web = $_POST['website'];
+$scope = $_POST['scope'];
+
+if(isset($_POST["name"])){
+	$insert = $con->prepare('INSERT INTO companiesBasic(id, name, website, scope) VALUES (?,?,?,?);');
+	$insert->bindValue(1,$id, PDO::PARAM_INT);
+	$insert->bindValue(2,$name, PDO::PARAM_STR,500);
+	$insert->bindValue(3,$web, PDO::PARAM_STR,500);
+	$insert->bindValue(4,$scope, PDO::PARAM_STR,500);
+	$insert->execute();
+}else{
+	echo "an error has occured!<br>";
+}
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Bug Bounty Insert Page</title>
+	<hr>
+	<center><h1><strong>Bug Bounty Insert Page</center></h1></center>
+	<link rel="stylesheet" href="landingPage1.css">
+	<link rel="stylesheet" href="dropdown.css">
+	<link rel="stylesheet" href="main.css">
+	<hr>
+</head>
+
+<body>
+	<?php setcookie("gotyounow",$_SESSION["username"]);?>
+	<hr>
+	<div class="navbar">
+		<a href="home.php">Home</a>
+		<a href="home.php">Profile</a>
+		<div class="dropdown">
+			<button class="dropbtn">Study forms
+				<i class="fa fa-caret-down"></i>
+			</button>
+			<div class="dropdown-content">
+				<a href="bb.php">Bug Bounty Entry</a>
+			</div>
+		</div>
+		<?php if(htmlspecialchars($_SESSION["username"]) == "admin"){echo"<a href='home.php'>Dashboard</a>";}?>
+		<div class="1">
+			<form action="logout.php" method="POST">
+				<center><input type="submit" value="logout" name="logout"></span></center>
+			</form>
+		</div>
+	</div>
+	<div class="page-header">
+		<center>Bug Bounty Entry</center>
+		<center><h1>ONLY ENETER COMPANIES WITH FULL SAFE HARBOR!!!!</h1></center>
+		<center><h1>ONLY ENETER COMPANIES WITH MONEY REWARDS!!!!</h1></center>
+	</div>	
+	<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
+		<center><table style="width 100%">
+			<tr>
+				<th>Enter the company name</th>
+				<th>Enter the company website</th>
+				<th>Enter the scope, with ; between the different sites</th>
+			</tr>
+			<tr>		
+				<th><input type="text" name="name" id="size1" placeholder="?"><br></th>
+				<th><input type="text" name="website" id="size2" placeholder="?"><br></th>
+				<th><input type="text" name="scope" id="size3" placeholder="?"><br></th>
+			</tr>
+		</table></center>
+		<center><input type="submit" value="submit"></center> 
+	</form>
+	</hr>
+	<center><p><strong>
+
+	</strong></p></center>
+</body>
+</html
