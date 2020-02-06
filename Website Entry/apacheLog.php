c094625236bc2a932f11fe50c8dab787740953ff
sschatz1997 <s.schatz1997@gmail.com>
2020-02-06 14:51:11 -0500

    Update apacheLog.php

--- Website Entry/apacheLog.php
+++ Website Entry/apacheLog.php
@@ -1,13 +1,16 @@
 <?php
 require_once "config.php";
 $log = "/var/log/apache2/access.log";
+$prep0 = $con->query('SELECT id FROM fromLogs WHERE logFile = "/var/log/syslog";');
+$prep0->execute();
+$count2 = $prep0->rowCount();
 $prep1 = $con->prepare('SELECT id from fromLogs WHERE logFile = ?;');
 $prep1->bindParam(1,$log,PDO::PARAM_STR,100);
 $prep1->execute();
 $entries = $prep1->fetch(PDO::FETCH_BOTH);
 //$entries = $prep1->fetchAll(PDO::FETCH_ASSOC);
 if(is_array($entries)){
-	$count = count($entries);
+	//$count = count($entries);
 //	echo "Their are : ". $count . "<br>";
 //	print_r($entries);
 }else{
@@ -33,7 +36,7 @@ $x = 0;
 $masterTime = [];
 //echo "<th>".$tt."<th>";
 $id = intval(array_pop($entries));
-while($x < $count){
+while($x < $count2){
 	$prep2->bindParam(1,$id,PDO::PARAM_INT);
 	$prep2->execute();
 	$ip = $prep2->fetch(PDO::FETCH_BOTH);
