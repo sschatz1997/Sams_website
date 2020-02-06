ddf60019a5d16d918e970f30370b39141607e3c3
sschatz1997 <s.schatz1997@gmail.com>
2020-02-06 14:08:23 -0500

    test

--- Website Entry/test1.php
+++ Website Entry/test1.php
@@ -1,11 +1,16 @@
 <?php
 require_once "config.php";
 $log = "/var/log/apache2/access.log";
-$prep1 = $con->prepare('SELECT logFile from fromLogs WHERE logFile = "?" order by id desc limit 1;');
+$prep1 = $con->prepare('SELECT ipAddr from fromLogs WHERE logFile = ?;');
 $prep1->bindParam(1,$log,PDO::PARAM_STR,100);
 $prep1->execute();
 $entries = $prep1->fetch(PDO::FETCH_BOTH);
-$count = count($entries);
-
-echo "Their are : ". $count;
+//$entries = $prep1->fetchAll(PDO::FETCH_ASSOC);
+if(is_array($entries)){
+	$count = count($entries);
+	echo "Their are : ". $count . "<br>";
+	print_r($entries);
+}else{
+	echo "is not an array " . gettype($entries) . "<br>";
+}
 ?>
