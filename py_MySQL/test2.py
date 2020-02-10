eac03ddca1b6d50adbc0f3e2c551bc77d5fce823
sschatz1997 <s.schatz1997@gmail.com>
2020-02-10 15:05:27 -0500

    Create test2.py

---
+++ py_MySQL/test2.py
@@ -0,0 +1,35 @@
+from functions import files
+
+
+def files1():
+	fs = ["auth.log", "syslog","ufw.log","access.log","error.log", "proftpd.log" ]
+	return fs
+
+def matchfileToExt(file):
+    # this does conversion
+    file2 = files()
+    if(file == "auth.log"):
+        filePath = file2[0]
+    elif(file == "syslog"):
+        filePath = file2[1]
+    elif(file == "ufw.log"):
+        filePath = file2[2]
+    elif(file == "access.log"):
+        filePath = files2[3]
+    elif(file == "error.log"):
+        filePath = files2[4]
+    elif(file == "proftpd.log"):
+        filePath = files2[5]
+    else:
+        filePath = "0"
+    
+    return filePath
+
+def matchExtToFile(file):
+    # this does conversion
+    file2 = files()
+    file3 = files1()
+    while(x != len(file3)):
+        if(file = file3[x]):
+            return file2[x]
+    return "0"
